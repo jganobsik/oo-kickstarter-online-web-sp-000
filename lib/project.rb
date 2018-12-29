207b31683c7489ef449622cb955071f4b24faf2b
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 13:54:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,7 @@ class Project
   def add_backer(backer)
     @backers << backer
     if backer.backed_projects.include?(self) != true
-    backer.add_project(self)
+    backer.back_project(self)
   end
   end
   
